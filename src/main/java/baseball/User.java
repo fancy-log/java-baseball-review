3235e73a5aa96ace791064b54d17f0113f187476
junyong <jymoon1121@gmail.com>
2022-12-04 16:14:09 +0900

    feat(User): 사용자로 부터 입력받는 클래스 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import baseball.model.Baseball;
+import baseball.util.Validate;
+import baseball.view.InputView;
+
+public class User {
+    private static final InputView inputView = new InputView();
+    private static final Validate validate = new Validate();
+    public Baseball getInput() {
+        String number = inputView.readNumber();
+        List<Integer> user = mappingToList(number);
+        validate.validateNumberRange(user);
+        return new Baseball(user);
+    }
+    private static List<Integer> mappingToList(String input) {
+        List<Integer> userNumber = new ArrayList<>();
+        for(int index = 0; index < input.length(); index++) {
+            int digit = strToInt(input, index);
+            userNumber.add(digit);
+        }
+        return userNumber;
+    }
+    private static int strToInt(String input, int index) {
+        char c = input.charAt(index);
+        int value = Character.getNumericValue(c);
+        return value;
+    }
+}
