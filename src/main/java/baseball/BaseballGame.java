b4ddf38a0f11d8aede23c4005b44a95b11da61ee
junyong <jymoon1121@gmail.com>
2022-12-04 16:45:48 +0900

    feat(BaseballGame): 최종 결과 및 재시작 여부 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,8 +8,11 @@ import baseball.model.Baseball;
 import baseball.util.Validate;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGame {
+    private static final int RESTART = 1;
+    private static final int QUIT = 2;
     private static final OutputView outputView = new OutputView();
     private static final InputView inputView = new InputView();
     private static final Validate validate = new Validate();
@@ -19,14 +22,22 @@ public class BaseballGame {
     private boolean flag = true;
     public void init() {
         Baseball computer = createComputerRandomNumber();
-        while(flag) {
+        while (flag) {
             Baseball user = inputUserBaseball();
             baseballResult = compareNumber(user, computer);
-            if(baseballResult.isAllCount()) {
-
+            if (baseballResult.isAllCount()) {
+                String result = baseballResult.getResult();
+                outputView.printAnswer(result);
+                outputView.printAllCorrect();
+                int restartAnswer = askRestart();
+                processRestartAnswer(restartAnswer);
+            } else {
+                String result = baseballResult.getResult();
+                outputView.printAnswer(result);
             }
         }
     }
+
     private Baseball createComputerRandomNumber() {
         return new Computer().createRandomNumber();
     }
@@ -34,8 +45,27 @@ public class BaseballGame {
         User user = new User();
         return user.getInput();
     }
-    public BaseballResult compareNumber(Baseball user, Baseball computer) {
+    private BaseballResult compareNumber(Baseball user, Baseball computer) {
         HashMap<String, Integer> check_result = checker.start(user, computer);
         return new BaseballResult(check_result);
+    }
+    private int askRestart() {
+        outputView.printRestart();
+        int reStart = getRestart();
+        return reStart;
+    }
+    private int getRestart() {
+        String s = Console.readLine();
+        int reStartNum = Integer.parseInt(s);
+        return reStartNum;
+    }
+    private void processRestartAnswer(int restart) {
+        validate.validateRestartOrQuit(restart);
+        if(restart == RESTART) {
+            init();
+        }
+        if(restart == QUIT) {
+            flag = false;
+        }
     }
 }
