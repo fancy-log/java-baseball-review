de25e20ae4d70eec038de654415b91cac97ddb59
junyong <jymoon1121@gmail.com>
2022-12-05 18:04:18 +0900

    fix(Application): try-catch문 제거(에러 처리)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,15 +6,10 @@ import baseball.view.OutputView;
 
 public class Application {
     private static final BaseballGame baseballGame = new BaseballGame();
-    private static InputView inputView = new InputView();
     private static OutputView outputView = new OutputView();
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        try {
-            init();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+        init();
     }
 
     private static void init() {
